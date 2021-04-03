344a4e2fe5c3d846cc8cdcb1b8168ce3906204a3
Arfian Bagus <arfian.nurmajid@idntimes.com>
2021-04-03 21:29:57 +0700

    fix convert

--- fetch-app/services/fetchservices.py
+++ fetch-app/services/fetchservices.py
@@ -14,15 +14,17 @@ class FetchService(ifetchservice.IFetchService):
 
         try:
             conversion = config.app['cache']['convert']
+            logging.info("no cache")
         except Exception as exc:
             logging.info('request conversi')
             resConvert = self.repo.fetchConversion()
             config.app['cache']['convert'] = resConvert["USD_IDR"]
+            conversion = resConvert["USD_IDR"]
+            logging.info("cache")
         
-        logging.info(resConvert)
         for i in res:
             if i["price"] != None:
-                i["price_usd"] = float(i["price"]) / resConvert["USD_IDR"]
+                i["price_usd"] = float(i["price"]) / conversion
             else:
                 i["price_usd"] = 0
         
